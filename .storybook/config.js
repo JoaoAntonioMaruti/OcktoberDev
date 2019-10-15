a15680b364735853a9064fec700f9e49cadc05d7
João Antonio <joaoantoniomaruti@gmail.com>
2019-10-15 20:03:21 -0300

    Comment code

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@storybook/react';
 
 const loadStories = () => {
-  require('./../src/components/Search/Search.story');
-  require('./../src/components/Logo/logo.story');
-  require('./../src/components/Button/Button.story');
-  require('./../src/containers/Main.story');
+//   require('./../src/components/Search/Search.story');
+//   require('./../src/components/Logo/logo.story');
+//   require('./../src/components/Button/Button.story');
+//   require('./../src/containers/Main.story');
 }
 
 configure(loadStories, module);
