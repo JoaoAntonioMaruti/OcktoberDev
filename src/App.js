373662886c45cf6fc70b7d95bf10daf261219c93
João Antonio <joaoantoniomaruti@gmail.com>
2019-10-15 20:02:31 -0300

    Comment code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import Main from './containers/Main'
+// import Main from './containers/Main'
 
 const App = () => {
-  return <Main />;
+  return <div />;
 }
+
+// replace with
+// const App = () => {
+//   return <div />;
+// }
 
 export default App;
